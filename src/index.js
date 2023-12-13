7558f5ac6412dd5fa9d17160c63312dfe3b193af
Gabriel <kunry3@hotmail.com>
2023-12-13 21:33:16 +0100

    feat: init jasmine

---
+++ src/index.js
@@ -0,0 +1,87 @@
+// Crea un nuevo array que contenga solo los títulos de las películas.
+
+function getTitles(movies) {}
+
+// Devuelve el título y el director de cada película.
+
+function getTitlesAndDirectors(movies) {}
+
+// Imprime el título de cada película que fue lanzada después del año 2000.
+
+function getTitlesAfter2000(movies) {}
+
+// Crea un nuevo array que contenga objetos con el título de la película y el año de lanzamiento.
+
+function getTitlesAndYears(movies) {}
+
+// Crea un nuevo array que contenga el número de géneros de cada película.
+
+function getNumberOfGenres(movies) {}
+
+// Crea un nuevo array que contenga solo las películas lanzadas después del año 2000.
+
+function filterMoviesAfter2000(movies) {}
+
+// Crea un nuevo array que contenga solo las películas dirigidas por ‘Christopher Nolan’.
+
+function filterNolanMovies(movies) {}
+
+// Crea un nuevo array que contenga solo las películas que son del género ‘Drama’.
+
+function filterDramaMovies(movies) {}
+
+// Ordena las películas por año de lanzamiento en orden ascendente Y si son iguales por el título.
+
+function sortByYear(movies) {}
+
+// Ordena las películas por el número de géneros en orden descendente, si son iguales por el año y si son del mismo año por el título.
+
+function sortByNumberOfGenres(movies) {}
+
+// Ordena las películas por el nombre del director en orden alfabético.
+
+function sortByDirector(movies) {}
+
+// Devuelve la suma total de todos los géneros de las películas.
+
+function getTotalNumberOfGenres(movies) {}
+
+// Encuentra la película con el título más largo.
+
+function getMovieWithLongestTitle(movies) {}
+
+// Encuentra el número de películas lanzadas cada año.
+
+function getNumberOfMoviesPerYear(movies) {}
+
+// Encuentra la primera película dirigida por ‘Quentin Tarantino’.
+
+function findFirstTarantinoMovie(movies) {}
+
+// Encuentra la primera película que es del género ‘Adventure’.
+
+function findFirstAdventureMovie(movies) {}
+
+// Encuentra la primera película lanzada antes de 2000.
+
+function findFirstMovieBefore2000(movies) {}
+
+// Encuentra el número de películas dirigidas por ‘Christopher Nolan’ que son del género ‘Drama’.
+
+function getNumberOfNolanDramaMovies(movies) {}
+
+// Encuentra el título de la película más antigua que es del género ‘Sci-Fi’.
+
+function getTitleOfOldestSciFiMovie(movies) {}
+
+// Encuentra el número de géneros únicos en todas las películas.
+
+function getNumberOfUniqueGenres(movies) {}
+
+// Encuentra el director que ha dirigido más películas.
+
+function getDirectorWithMostMovies(movies) {}
+
+// Encuentra el año con el mayor número de películas lanzadas.
+
+function getYearWithMostMovies(movies) {}
